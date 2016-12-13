e9e428604175170ee541e87f7e7153c8e5c7ed2b
zhao liang <leonchaoz@gmail.com>
2016-12-13 15:54:43 +1000

    Add project files.

---
+++ HWebServer/server.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+require('./app/routes')(app);
+
+app.listen(port);
+
+// shoutout to the user                     
+console.log('Magic happens on port ' + port);
+//commite
+// expose app           
+exports = module.exports = app;                         
+//dsfsdf
